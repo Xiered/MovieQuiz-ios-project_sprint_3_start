7e2a79fa3a9a43b22abd6461c9a91eb71fcbfaf1
Xiered <vnyantost@gmail.com>
2023-08-20 22:43:51 +0300

    Добавил слабые ссылки weak self для алерты action = UIAlertAction и DispatchQueue.main.asyncAfter

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -129,7 +129,8 @@ final class MovieQuizViewController: UIViewController {
             imageView.layer.borderWidth = 8
             imageView.layer.cornerRadius = 20
             imageView.layer.borderColor = isCorrect ? UIColor(named: "YPRed")?.cgColor : UIColor(named: "YPRed")?.cgColor
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+                guard let self = self else { return }
                 self.showNextQuestionOrResults()
             }
         }
@@ -161,7 +162,8 @@ final class MovieQuizViewController: UIViewController {
             message: result.text,
             preferredStyle: .alert)
         
-        let action = UIAlertAction(title: result.buttonText, style: .default) {_ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self ] _ in
+            guard let self = self else { return }
             self.currentQuestionIndex = 0
             
             // скидываем счётчик правильных ответов
