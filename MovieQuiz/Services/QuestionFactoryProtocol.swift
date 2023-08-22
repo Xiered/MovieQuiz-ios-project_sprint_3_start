bcd654c7a23dbc792aa9bdb60c640f190cfa9f33
Xiered <vnyantost@gmail.com>
2023-08-22 16:23:22 +0300

    Создание протокола QuestionFactoryProtocol в отдельном Swift-файле с аналогичным названием

---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Герасимов on 22.08.2023.
+//
+
+import Foundation
+
+protocol QuestionFactoryProtocol {
+    func requestNextQuestion() -> QuizQuestion?
+}
