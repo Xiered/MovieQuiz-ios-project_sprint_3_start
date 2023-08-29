38ea403224092c505f9b0b4e9968faabccda8df1
Xiered <vnyantost@gmail.com>
2023-08-29 14:52:50 +0300

    Добавление перечисления с ключами всех сущностей, добавление конструкции guard-let в сеттер

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -13,6 +13,10 @@ final class StatisticServiceTempImplementation: StatisticService {
     
     private let userDefaults = UserDefaults.standard
     
+    private enum Keys: String {
+        case correct, total, bestGame, gamesCount
+    }
+    
     var totalAccuracy: Double = 0.0
     
     var gamesCount: Int = 0
@@ -22,7 +26,11 @@ final class StatisticServiceTempImplementation: StatisticService {
         
         }
         set {
-            
+            guard let data = JSONEncoder().encode(newValue) else {
+                print("Невозможно сохранить результат")
+                return
+            }
+            userDefaults.set(data, forKey: Keys.bestGame.rawValue)
         }
     }
     
