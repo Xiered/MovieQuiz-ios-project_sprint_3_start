4b0e716ae86cb6cd9fe9a3e4f6ec2352a8cf1463
Xiered <vnyantost@gmail.com>
2023-08-29 13:48:53 +0300

    Создание Swift файлов в папке Service

---
+++ MovieQuiz/Services/StatisticService.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticService.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Герасимов on 29.08.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/StatisticServiceProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  StatisticServiceProtocol.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Герасимов on 29.08.2023.
+//
+
+import Foundation
