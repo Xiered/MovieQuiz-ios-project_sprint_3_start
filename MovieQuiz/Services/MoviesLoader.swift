2dca7283a21992ed70aea81db2ce4dde8261254a
Xiered <vnyantost@gmail.com>
2023-09-02 15:34:20 +0300

    Создание URL метода, который позволяет получить список самых популярных фильмов

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -12,7 +12,16 @@ protocol MoviesLoading {
 }
 
 struct MoviesLoader: MoviesLoading {
+    // MARK: - NetworkClient
     private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://imdb-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+        }
+        return url
+    }
     
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
         
