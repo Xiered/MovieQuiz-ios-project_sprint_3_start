89b2a475a9fb4a8c50832bf3290232c71a15fd9c
Xiered <vnyantost@gmail.com>
2023-09-02 14:46:20 +0300

    Создание структур по компоновке JSON формата

--- MovieQuiz/Models/MostPopularMovies.swift
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct MostPopularMovies {
+    let errorMessage: String
+    let items: [MostPopularMovie]
+}
+
+struct MostPopularMovie {
+    let title: String
+    let rating: String
+    let imageURL: URL
+}
