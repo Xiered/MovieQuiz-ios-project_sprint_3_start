cbfc0d8c46d30b3c58f665b72b74ea41ed71433a
Xiered <134545303+Xiered@users.noreply.github.com>
2023-09-16 17:03:57 +0300

    Adding unit-test for MoviesLoader

---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,26 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Дмитрий Герасимов on 16.09.2023.
+//
+
+import Foundation
+import XCTest
+@testable import MovieQuiz
+
+
+class MovieLoaderTests: XCTestCase {
+    
+    func testSuccessLoading() {
+        // When
+        // Given
+        // Then
+    }
+    
+    func testFailureLoading() {
+        // When
+        // Given
+        // Then
+    }
+}
