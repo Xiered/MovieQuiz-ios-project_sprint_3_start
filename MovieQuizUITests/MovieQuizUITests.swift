7ad12edc3cd6ca0015ff7f072deade3612a38f19
Xiered <134545303+Xiered@users.noreply.github.com>
2023-09-18 01:03:18 +0300

    Create launch and terminate

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -12,17 +12,21 @@ final class MovieQuizUITests: XCTestCase {
     var app: XCUIApplication!
 
     override func setUpWithError() throws {
+        try super.setUpWithError()
         
         app = XCUIApplication()
+        app.launch()
         
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDownWithError() throws {
+        try super.tearDownWithError()
         
-      app = nil
+        app.terminate()
+        app = nil
         
     }
 
