51ce39e99be60aaec56c0754eefeac05472549dc
Xiered <134545303+Xiered@users.noreply.github.com>
2023-09-23 20:57:22 +0300

    Create File and Class MovieQuizPresenter

---
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  MovieQuizPresenter.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Герасимов on 23.09.2023.
+//
+
+import Foundation
+import UIKit
+
+final class MovieQuizPresenter {
+    
+}
